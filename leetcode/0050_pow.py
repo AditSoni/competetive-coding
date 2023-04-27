3d0e05da58a5eb5966032a847238897b1c2a4114
Adit Soni <aditsoni@myhost.local>
2023-04-27 14:12:32 +0530

    power function

---
+++ leetcode/0050_pow.py
@@ -0,0 +1,23 @@
+def myPow(self, x: float, n: int) -> float:
+
+        def function(base=x, exponent=abs(n)):
+            if exponent == 0:
+                return 1
+            elif exponent % 2 == 0:
+                return function(base * base, exponent // 2)
+            else:
+                return base * function(base * base, (exponent - 1) // 2)
+
+        f = function()
+        
+        return float(f) if n >= 0 else 1/f
+
+"""
+
+x**n :
+
+if n is even:
+(x**2) ** (n/2)
+if n is odd:
+x*((x**2)**(n-1/2))
+"""
