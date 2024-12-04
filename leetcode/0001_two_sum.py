a4758352cf96b180daabe9a672bf50addbc129e0
Adit Soni <aditsoni@Adits-MacBook-Pro.local>
2024-12-04 16:14:48 +0530

    Updated 0001_two_sum.py

--- leetcode/0001_two_sum.py
+++ leetcode/0001_two_sum.py
@@ -1,11 +1,16 @@
 def two_sum(num,target):
     map = {}
+    results = []
     for i in range(len(num)):
-        if num[i] not in map:
-            map[target - num[i]] = i
+        if num[i] not in map:     # store the key complement (sum - num) and value as current index 
+            map[target - num[i]] = i  # when an item we need is in the dict ; we have found the pair ; we return the value at that key and the current index
         else:
-            return map[num[i]], i 
+            results.append((map[num[i]], i ))
 
-    return -1, -1
+    if not results:
+        return -1, -1
+    else :
+        return results
 
-print(two_sum([2,3,11,6,15,7],9))
+print(two_sum([2,3,11,6,15,7],90))
+
